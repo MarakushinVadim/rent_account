5a73bc01e61397eb51b141e85d83fc3069c91493
Vadim <vadim.marakushin@gmail.com>
2024-11-15 15:08:02 +0300

    start

--- rent/models.py
+++ rent/models.py
@@ -1,13 +1,14 @@
-from tkinter.constants import CASCADE
+from django.utils import timezone
 
 from django.db import models
+from django.db.models import CASCADE
 
 NULLABLE = {'null':True, 'blank':True}
 
-class Company:
+class Company(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя/Название', help_text='Введиет название компании или имя ч/л')
     phone = models.CharField(max_length=30, verbose_name='Номер телефона', help_text='Введите номер телефона для связи')
-    date = models.DateField(verbose_name='Дата начала арендного срока', help_text='Введите дату начала арендного срока')
+
 
     def __str__(self):
         return self.name
@@ -16,7 +17,7 @@ class Company:
         verbose_name = 'Компания'
         verbose_name_plural = 'Компании'
 
-class Container:
+class Container(models.Model):
     class AgeChoices(models.TextChoices):
         UNDER_5_RU = 'under_5_ru', 'Моложе 5 лет, РФ'
         UNDER_5_CH = 'under_5_ch', 'Моложе 5 лет, Китай'
@@ -24,10 +25,12 @@ class Container:
         OVER_5_CH = 'over_5_ch', 'Старше 5 лет Китай'
 
     name = models.CharField(max_length=50, verbose_name='Наименование баллона', help_text='Введите наименование баллона')
-    company = models.ForeignKey(Company, on_delete=CASCADE)
+    company = models.ForeignKey(Company, on_delete=CASCADE, verbose_name='Клиент')
     age_group = models.CharField(max_length=10, choices=AgeChoices.choices, verbose_name='Вид тары')
-    count = models.IntegerField(verbose_name='Количество', help_text='Введите количество баллонов')
+    count_shipped = models.IntegerField(**NULLABLE, verbose_name='Количество отгружено', help_text='Введите количество отгруженых баллонов')
+    count_taken = models.IntegerField(**NULLABLE, verbose_name='Количество принято', help_text='Введите количество принятых баллонов')
     duration = models.CharField(max_length=255, **NULLABLE, verbose_name='Срок аренды', help_text='Предположительный срок арены')
+    date = models.DateField(default=timezone.now, verbose_name='Дата', help_text='Введите дату')
 
     def __str__(self):
         return self.name
@@ -35,3 +38,6 @@ class Container:
     class Meta:
         verbose_name = 'Баллон'
         verbose_name_plural = 'Баллоны'
+
+    class Nomenclature(models.Model):
+        name = models.CharField(max_length=255, verbose_name='Наименование баллона')
--- rent/urls.py
+++ rent/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from rent.views import BaseView
+from rent.views import BaseView, CompanyCreateView, ContainerCreateView, ContainerListView
 
 app_name = 'rent'
 
 urlpatterns = [
-    path('', BaseView.as_view(), name='base')
+    path('', BaseView.as_view(), name='base'),
+    path('company_create', CompanyCreateView.as_view(), name='company_create'),
+    path('container_create', ContainerCreateView.as_view(), name='container_create'),
+    path('container_list', ContainerListView.as_view(), name='container_list'),
 ]
